1ba389c5c06648ee4e455760b4f9d6da8a6381b2
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-15 21:19:45 +0700

    iyye

---
+++ belajar tuple.py
@@ -0,0 +1,12 @@
+#mencari nilai terkecil daN terbesar di tuple
+
+nilai = (1, 2, 5,7,9,12)
+
+print("nilai terbesar",max(nilai))
+print("nilai terkecil",min(nilai))
+
+print("="*30)
+
+#perulangan pada tuple
+for x in nilai:
+    print(x)
